1aa42996fa3dcea354102e759d3548ce654310c5
Sai <sai@SAIs-MacBook-Pro.local>
2023-08-22 12:32:11 +0530

    backend prodution ready

--- homescountbackend-node/config/allowedOrigins.js
+++ homescountbackend-node/config/allowedOrigins.js
@@ -1,6 +1,3 @@
 export const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000',
+    'https://homescout.onrender.com'
 ];
